6231d769b8b7c79700f97ffaf38f2b358d3f8cb9
kovut <kovut@DESKTOP-MU0LG6N>
2016-10-27 19:05:11 +0100

    Cálculo de un único vector.
    FIN

--- src/pr3p4java/Pr3P4Java.java
+++ src/pr3p4java/Pr3P4Java.java
@@ -3,14 +3,12 @@ package pr3p4java;
 public class Pr3P4Java {
 
     public static void main(String[] args) {
-        int[] tamaño = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
-        for (int i = 0; i < tamaño.length; i++) {
-            DatosEstadisticos de = new DatosEstadisticos();
-            int[] vec = GeneraCaso.generaVector(tamaño[i], true);
-            Ordenar1Vector a = new Ordenar1Vector();
-            a.ordena(vec, de);
-            CompruebaCaso.compruebaVector(vec, vec.length);
-            System.out.println("Vector de " + tamaño[i] + " elementos - Tarda " + de.dameTiempo() + " segundos.");
-        }
+        int tamaño = 25000;
+        DatosEstadisticos de = new DatosEstadisticos();
+        int[] vec = GeneraCaso.generaVector(tamaño, false);
+        Ordenar1Vector a = new Ordenar1Vector();
+        a.ordena(vec, de);
+        CompruebaCaso.compruebaVector(vec, vec.length);
+        System.out.println("Vector de " + tamaño + " elementos - Tarda " + de.dameTiempo() + " segundos.");
     }
 }
